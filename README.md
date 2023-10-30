e5cc97e5b2da25dd3d2d24ae3f8aa79cc87d78b3
cam <cam@cams-MacBook-Pro.local>
2023-10-30 11:38:39 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # face_db
+
+Simple script to generate a database of face embeddings from [the FFHQ dataset](https://github.com/NVlabs/ffhq-dataset).
+
+
+## Steps
+
+Download [the dataset](https://www.kaggle.com/datasets/arnaud58/flickrfaceshq-dataset-ffhq/).
+
+Install all `requirements.txt`.
+
+Run the database generating script like this: `python create_embedding_db.py path/to/face/images`.
+
+
+## Properties
+
+The resulting sqlite database will be named `face_embeddings.db`.
+
+It will look like this:
+
+```
+id | embedding
+```
+
+And you can inspect it like this:
+
+```
+> sqlite3 face_embeddings.db
+> .tables
+> SELECT * FROM faces LIMIT 10;
+> .exit
+```
+
+Enjoy!
---
+++ create_embedding_db.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import sqlite3
+import face_recognition
+
+
+
+# Path to all the face pics.
+FACE_PIC_BASE_PATH = sys.argv[1]
+
+
+def create_face_embedding(image: str) -> list:
+    """
+    Accepts a path to an image file and returns an embedding as a numpy array.
+
+    Parameters
+    ----------
+    image : str
+        The path to an image file.
+
+    Returns
+    -------
+    list
+        The image embedded as a 128-dimension vector.
+    """
+    image_file = face_recognition.load_image_file(image)
+    try:
+        embedding = face_recognition.face_encodings(image_file)[0]
+    except IndexError:
+        return None
+
+    return embedding
+
+
+# Connect to a database.
+con = sqlite3.connect("face_embeddings.db")
+cur = con.cursor()
+
+# Create faces table if it doesn't already exist
+try:
+    cur.execute("CREATE TABLE faces (id VARCHAR(255), embedding VARCHAR(255), UNIQUE(id))")
+except sqlite3.OperationalError:
+    pass
+
+
+# Insert every file into the database
+for file in sorted(os.listdir(FACE_PIC_BASE_PATH)):
+    file_path = os.path.join(FACE_PIC_BASE_PATH, file)
+
+    # Sometimes an embedding can't be created.
+    try:
+        embedding = create_face_embedding(file_path)
+    except IndexError:
+        embedding = None
+    
+    # Ignore file if already uploaded.
+    sql = """
+        INSERT OR IGNORE INTO faces(id, embedding)
+        VALUES(?, ?)
+        """
+    cur.execute(sql, (file, str(embedding))) # convert the embedding vector to a string.
+    con.commit()
